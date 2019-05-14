2adb8e5e6fda2906bef8fe1ce51575312a415496
KagaK2 <KhanhPhan2512@gmail.com>
2019-05-14 17:46:51 +0300

    second draft, static page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "../styles/App.css";
 import logo from "../img/sos-logo.png";
-
+import { ReactComponent as Facebook } from "../img/Facebook.svg";
+import { ReactComponent as Instagram } from "../img/Instagram.svg";
+import { ReactComponent as Twitter } from "../img/Twitter.svg";
 function App() {
   return (
     <div className="App">
@@ -17,6 +19,11 @@ function App() {
         <a className="sos-info-button link-button">MORE INFO</a>
         <a className="sos-noti-button link-button">GET NOTIFIED</a>
       </div>
+      <footer>
+        <Facebook />
+        <Instagram />
+        <Twitter />
+      </footer>
     </div>
   );
 }
